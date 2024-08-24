f3884c0ab0182df0f34a67f8678041a4b8fc9326
Prince Jodhani <princejodhani21@gmail.com>
2024-08-24 19:57:54 +0530

    changes done server actions for editprofile is done but little bugs

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -60,12 +60,8 @@ function ProfileForm({ username }: ProfileFormProps) {
   const { control, handleSubmit, register } = useForm<FormValues>({
 
     defaultValues: {
-<<<<<<< HEAD
   
       fullname: username,
-=======
-      fullname: username, // Set default value for firstname
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
       bio: "",
       insta: "",
       facebook: "",
@@ -79,12 +75,8 @@ function ProfileForm({ username }: ProfileFormProps) {
   });
 
   const [isPsychiatrist, setIsPsychiatrist] = useState(false);
-<<<<<<< HEAD
   const [isPsychologist, setIsPsychologist] = useState(false);
   const [profileImagePreview, setProfileImagePreview] = useState<string | null>(null);
-=======
-  const [isPsychologist, setIspsychologist] = useState(false);
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
 
   const onProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -140,7 +132,6 @@ function ProfileForm({ username }: ProfileFormProps) {
   };
 
   return (
-<<<<<<< HEAD
     <Card className="w-full mx-auto">
       <CardHeader>
         <CardTitle className="text-center text-2xl">
@@ -195,53 +186,12 @@ function ProfileForm({ username }: ProfileFormProps) {
 
           <FormField
             control={control}
-=======
-    <Card className="w-full max-w-xl mx-auto shadow-lg rounded-xl overflow-hidden">
-      <CardHeader className="bg-blue-500 text-white">
-        <CardTitle className="text-center text-3xl font-semibold">
-          Profile Information
-        </CardTitle>
-        <CardDescription className="text-center text-base mt-1  text-white">
-          Update your profile details
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="p-6 space-y-6">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={control}
-            name="fullname"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg font-medium text-gray-700">
-                  Full Name
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                    placeholder="Full Name"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={control}
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
             name="bio"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-lg font-medium text-gray-700">
-                  Bio
-                </FormLabel>
+                <FormLabel>Bio</FormLabel>
                 <FormControl>
-                  <Textarea
-                    className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                    placeholder="Brief bio"
-                    {...field}
-                  />
+                  <Textarea placeholder="Brief bio" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -253,15 +203,8 @@ function ProfileForm({ username }: ProfileFormProps) {
             name="insta"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-lg font-medium text-gray-700">
-                  Instagram
-                </FormLabel>
                 <FormControl>
-                  <Input
-                    className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                    placeholder="Instagram URL"
-                    {...field}
-                  />
+                  <Input placeholder="Instagram URL" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -273,15 +216,8 @@ function ProfileForm({ username }: ProfileFormProps) {
             name="facebook"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-lg font-medium text-gray-700">
-                  Facebook
-                </FormLabel>
                 <FormControl>
-                  <Input
-                    className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                    placeholder="Facebook URL"
-                    {...field}
-                  />
+                  <Input placeholder="Facebook URL" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -293,31 +229,25 @@ function ProfileForm({ username }: ProfileFormProps) {
             name="twitter"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-lg font-medium text-gray-700">
-                  Twitter
-                </FormLabel>
                 <FormControl>
-                  <Input
-                    className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                    placeholder="Twitter URL"
-                    {...field}
-                  />
+                  <Input placeholder="Twitter URL" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-          <div className="flex items-center justify-between space-x-4 p-4 rounded-md border bg-gray-50">
+          <div className="flex items-center justify-between space-x-4 rounded-md border p-4 bg-muted">
             <div className="flex-1 space-y-1">
-              <p className="text-lg font-semibold leading-none text-gray-700">
+              <p className="text-sm font-medium leading-none">
                 Are you a Psychiatrist?
               </p>
-              <p className="text-sm text-gray-500">Sign Up as a Psychiatrist</p>
+              <p className="text-sm text-muted-foreground">
+                Sign Up as a Psychiatrist
+              </p>
             </div>
             <Switch
               checked={isPsychiatrist}
-<<<<<<< HEAD
               onCheckedChange={handlePsychiatristChange}
             />
           </div>
@@ -333,24 +263,6 @@ function ProfileForm({ username }: ProfileFormProps) {
             <Switch
               checked={isPsychologist}
               onCheckedChange={handlePsychologistChange}
-=======
-              onCheckedChange={setIsPsychiatrist}
-              className="focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div className="flex items-center justify-between space-x-4 p-4 rounded-md border bg-gray-50">
-            <div className="flex-1 space-y-1">
-              <p className="text-lg font-semibold leading-none text-gray-700">
-                Are you a Psychologist?
-              </p>
-              <p className="text-sm text-gray-500">Sign Up as a Psychologist</p>
-            </div>
-            <Switch
-              checked={isPsychologist}
-              onCheckedChange={setIspsychologist}
-              className="focus:ring-2 focus:ring-blue-500"
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
             />
           </div>
 
@@ -361,17 +273,10 @@ function ProfileForm({ username }: ProfileFormProps) {
                 name="degreeName"
                 render={() => (
                   <FormItem>
-                    <FormLabel className="text-lg font-medium text-gray-700">
-                      Qualification
-                    </FormLabel>
+                    <FormLabel>Qualification</FormLabel>
                     <FormControl>
-<<<<<<< HEAD
                       <Select>
                         <SelectTrigger className="w-full">
-=======
-                      <Select {...field}>
-                        <SelectTrigger className="w-full border-gray-300 focus:ring-2 focus:ring-blue-500">
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
                           <SelectValue placeholder="Select a Qualification" />
                         </SelectTrigger>
                         <SelectContent>
@@ -391,69 +296,48 @@ function ProfileForm({ username }: ProfileFormProps) {
                   </FormItem>
                 )}
               />
-
               <FormField
                 control={control}
                 name="degreeFile"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-lg font-medium text-gray-700">
-                      Degree File
-                    </FormLabel>
+                    <FormLabel>Degree File</FormLabel>
                     <FormControl>
                       <Input
                         id="degreeFile"
                         type="file"
                         {...register("degreeFile")}
-<<<<<<< HEAD
-=======
-                        className="focus:ring-2 focus:ring-blue-500"
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-
               <FormField
                 control={control}
                 name="registration"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-lg font-medium text-gray-700">
-                      Registration Certificate
-                    </FormLabel>
+                    <FormLabel>Registration Certificate</FormLabel>
                     <FormControl>
                       <Input
                         id="registration"
                         type="file"
                         {...register("registration")}
-<<<<<<< HEAD
-=======
-                        className="focus:ring-2 focus:ring-blue-500"
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-
               <FormField
                 control={control}
                 name="website"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-lg font-medium text-gray-700">
-                      Website URL
-                    </FormLabel>
+                    <FormLabel>Website URL</FormLabel>
                     <FormControl>
-                      <Input
-                        className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                        placeholder="Website URL"
-                        {...field}
-                      />
+                      <Input placeholder="Website URL" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -462,7 +346,6 @@ function ProfileForm({ username }: ProfileFormProps) {
             </>
           )}
 
-<<<<<<< HEAD
           {isPsychologist && (
             <FormField
               control={control}
@@ -481,39 +364,6 @@ function ProfileForm({ username }: ProfileFormProps) {
 
           <CardFooter className="flex justify-between pt-4">
             <Button onClick={handleSubmit(onSubmit)} type="submit">Update</Button>
-=======
-{isPsychologist && (
-            <>
-              <FormField
-                control={control}
-                name="website"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-lg font-medium text-gray-700">
-                      Website URL
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border-gray-300 focus:ring-2 focus:ring-blue-500"
-                        placeholder="Website URL"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </>
-          )}
-
-          <CardFooter className="flex justify-between pt-6">
-            <Button
-              type="submit"
-              className="w-full py-3 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition-all"
-            >
-              Update Profile
-            </Button>
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
           </CardFooter>
         </form>
       </CardContent>
@@ -535,10 +385,7 @@ export function Profile({ username }: { username: string }) {
           degreeFile: null,
           registration: null,
           website: "",
-<<<<<<< HEAD
           profileImage: null,
-=======
->>>>>>> 44504e02b57c9bf3fe649178f273c4fe4814e3af
         },
       })}
     >
