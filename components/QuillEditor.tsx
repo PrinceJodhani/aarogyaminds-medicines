bace59fc52a6b057457419d78f9e6c862d8ad97d
Prince Jodhani <princejodhani21@gmail.com>
2024-10-08 14:34:13 +0530

    done!

--- components/QuillEditor.tsx
+++ components/QuillEditor.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
-import { Label } from "@/components/ui/label"
-import Quill from 'quill';
-// Dynamically import ReactQuill to avoid SSR issues
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+// import React from 'react';
+// import dynamic from 'next/dynamic';
+// import 'react-quill/dist/quill.snow.css';
+// import { Label } from "@/components/ui/label"
+// import Quill from 'quill';
+// // Dynamically import ReactQuill to avoid SSR issues
+// const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
-interface QuillEditorProps {
-  value: string;
-  onChange: (value: string) => void;
-}
+// interface QuillEditorProps {
+//   value: string;
+//   onChange: (value: string) => void;
+// }
 
-const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange }) => {
-  return (
-    <div className="flex flex-col space-y-1.5">
+// const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange }) => {
+//   return (
+//     <div className="flex flex-col space-y-1.5">
 
-      <ReactQuill
-        value={value}
-        onChange={onChange}
-        theme="snow"
-        placeholder="Write your blog content here..."
-        modules={{
-            toolbar: [
-                [{ 'font': [] }],
-                [{ 'size': ['small', 'medium', 'large', 'huge'] }],
-                ['bold', 'italic', 'underline', 'strike'],
-                [{ 'color': [] }, { 'background': [] }],
-                [{ 'script': 'sub' }, { 'script': 'super' }],
-                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                [{ 'indent': '-1' }, { 'indent': '+1' }],
-                [{ 'align': [] }],
-                ['link', 'image', 'video'],
-                ['clean'] // Remove formatting button
-              ],
-          }}
-          style={{ height: '400px' }}
-      />
-    </div>
-  );
-};
+//       <ReactQuill
+//         value={value}
+//         onChange={onChange}
+//         theme="snow"
+//         placeholder="Write your blog content here..."
+//         modules={{
+//             toolbar: [
+//                 [{ 'font': [] }],
+//                 [{ 'size': ['small', 'medium', 'large', 'huge'] }],
+//                 ['bold', 'italic', 'underline', 'strike'],
+//                 [{ 'color': [] }, { 'background': [] }],
+//                 [{ 'script': 'sub' }, { 'script': 'super' }],
+//                 [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+//                 [{ 'indent': '-1' }, { 'indent': '+1' }],
+//                 [{ 'align': [] }],
+//                 ['link', 'image', 'video'],
+//                 ['clean'] // Remove formatting button
+//               ],
+//           }}
+//           style={{ height: '400px' }}
+//       />
+//     </div>
+//   );
+// };
 
-export default QuillEditor;
+// export default QuillEditor;
